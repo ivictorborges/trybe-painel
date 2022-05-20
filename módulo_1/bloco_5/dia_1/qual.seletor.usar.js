3ef3e586275250f2d48fc35af8ac24d288eb30cc
Victor <v_tord@hotmail.com>
2022-05-19 23:16:55 -0300

    Inclusão de arquivos .html, .js e .css

---
+++ módulo_1/bloco_5/dia_1/qual.seletor.usar.js
@@ -0,0 +1,25 @@
+let header = document.querySelector('#header-container');
+    header.style.backgroundColor = 'lightgreen';
+
+let topTitle = document.querySelector('h1');
+    topTitle.innerText = 'Administrador do Tempo da Trybe';
+    topTitle.style.backgroundColor = 'lightgreen';
+
+let emergencyTasks = document.querySelector('.emergency-tasks');
+    emergencyTasks.style.backgroundColor = 'pink';
+
+let emergencyTitleOne = document.querySelectorAll('h3')[0];
+    emergencyTitleOne.style.backgroundColor = 'purple';
+let emergencyTitleTwo = document.querySelectorAll('h3')[1];
+    emergencyTitleTwo.style.backgroundColor = 'purple';
+
+let noEmergencyTasks = document.querySelector('.no-emergency-tasks');
+    noEmergencyTasks.style.backgroundColor = 'yellow';
+
+let noEmergencyTitleOne = document.querySelectorAll('h3')[2];
+    noEmergencyTitleOne.style.backgroundColor = 'black';
+let noEmergencyTitleTwo = document.querySelectorAll('h3')[3];
+    noEmergencyTitleTwo.style.backgroundColor = 'black';
+
+let footContainer = document.querySelector('#footer-container');
+    footContainer.style.backgroundColor = 'darkgreen';
