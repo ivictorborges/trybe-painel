06d78cc9c5441dbeac5973cd84392b9fed55f728
Victor <v_tord@hotmail.com>
2022-05-24 11:14:31 -0300

    Inclusão de arquivos JS sobre Dia 2

---
+++ módulo_1/bloco_4/dia_2/arrays.js
@@ -0,0 +1,9 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+let menuServices = menu[1];
+
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+menu[4] = 'Contato';
+
+console.log(menu.sort());
---
+++ módulo_1/bloco_4/dia_2/for-of.js
@@ -0,0 +1,7 @@
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+names[4] = 'Victor';
+
+for ( var name of names) {
+    console.log(name);
+}
