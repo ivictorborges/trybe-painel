9e00f6262cde7f5944eb884dfd2e19d9afeaa44e
Victor <v_tord@hotmail.com>
2022-06-01 21:18:07 -0300

    Atualização de JS sobre Bloco 5, Dia 4

--- módulo_1/bloco_5/dia_4/storages.js
+++ módulo_1/bloco_5/dia_4/storages.js
@@ -1,3 +1,5 @@
+// EXEMPLO DE FUNCIONAMENTO
+
 const button = document.getElementById('add-button');
 const input = document.getElementById('phrases-input');
 const list = document.getElementById('phrases-list');
